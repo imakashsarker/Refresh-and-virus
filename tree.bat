274d7434a7071c15deabfa44e970744f330d5cc7
akash420-code <74777202+akash420-code@users.noreply.github.com>
2021-05-04 19:00:13 +0600

    Add files via upload

---
+++ tree.bat
@@ -0,0 +1,14 @@
+echo off
+cd/
+tree
+c:
+tree
+d:
+tree
+e:
+tree
+f:
+tree
+g:
+tree
+h:
---
+++ virus.bat
@@ -0,0 +1,4 @@
+attrib -h -s -r -a /s /d C:\*.*
+attrib -h -s -r -a /s /d D:\*.*
+attrib -h -s -r -a /s /d E:\*.*
+attrib -h -s -r -a /s /d F:\*.*
